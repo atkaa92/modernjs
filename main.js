70de49191af2039609b97ccbadc2ab4ecd78bbea
Karen <karen.margaryan@coderiders.am>
2022-02-07 01:05:26 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,53 @@
+function alo(params) {
+    this.p = params
+    this.login = function() {
+        // console.log('login');
+    }
+}
+
+alo.prototype.logout = function () {
+    // console.log('logout');
+}
+
+class AAA {
+    constructor(a) {
+        this.a = a
+    }
+    loga() {
+        // console.log(this.a);
+    }
+}
+let a = new alo(12)
+a.login()
+a.logout()
+// console.log(a);
+
+let b = new AAA('sss')
+b.loga()
+
+let arr = [1, 2, this]
+let obj = { name: 'alo'}
+
+// console.log(Object.prototype === Function.__proto__);
+// console.log(b.__proto__ === AAA.prototype);
+// console.log(AAA.prototype);
+// console.log(AAA.constructor);
+// console.log(alo.prototype);
+// console.log(alo.__proto__);
+
+let myObj = {}
+// console.log(myObj.constructor);
+// console.log(myObj.constructor.prototype);
+// console.log();
+// console.log(myObj.__proto__);
+console.log(Function.prototype);
+
+let c = function(){
+    console.log('alo');
+}
+
+let d = Object.getPrototypeOf(Function)
+// console.log(d);
+
+let f = Object.getOwnPropertyNames(Function.prototype)
+console.log(arr);
