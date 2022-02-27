734e98b266ec63d3ae5b5aab837545c91fca3612
Karen <atkaa92@gmail.com>
2022-02-27 21:23:25 +0400

    alo

--- main.js
+++ main.js
@@ -1,32 +1,32 @@
-function alo(params) {
-    this.p = params
-    this.login = function() {
-        // console.log('login');
-    }
-}
+// function alo(params) {
+//     this.p = params
+//     this.login = function() {
+//         // console.log('login');
+//     }
+// }
 
-alo.prototype.logout = function () {
-    // console.log('logout');
-}
+// alo.prototype.logout = function () {
+//     // console.log('logout');
+// }
 
-class AAA {
-    constructor(a) {
-        this.a = a
-    }
-    loga() {
-        // console.log(this.a);
-    }
-}
-let a = new alo(12)
-a.login()
-a.logout()
+// class AAA {
+//     constructor(a) {
+//         this.a = a
+//     }
+//     loga() {
+//         // console.log(this.a);
+//     }
+// }
+// let a = new alo(12)
+// a.login()
+// a.logout()
 // console.log(a);
 
-let b = new AAA('sss')
-b.loga()
+// let b = new AAA('sss')
+// b.loga()
 
-let arr = [1, 2, this]
-let obj = { name: 'alo'}
+// let arr = [1, 2, this]
+// let obj = { name: 'alo'}
 
 
 // console.log(Object.prototype === Function.__proto__);
@@ -36,19 +36,53 @@ let obj = { name: 'alo'}
 // console.log(alo.prototype);
 // console.log(alo.__proto__);
 
-let myObj = {}
+// let myObj = {}
 // console.log(myObj.constructor);
 // console.log(myObj.constructor.prototype);
 // console.log();
 // console.log(myObj.__proto__);
-console.log(Function.prototype);
+// console.log(Function.prototype);
 
-let c = function(){
-    console.log('alo');
-}
+// let c = function(){
+//     console.log('alo');
+// }
 
-let d = Object.getPrototypeOf(Function)
+// let d = Object.getPrototypeOf(Function)
 // console.log(d);
 
-let f = Object.getOwnPropertyNames(Function.prototype)
-console.log(arr);
+// let f = Object.getOwnPropertyNames(Function.prototype)
+// console.log(arr);
+
+// var alo = 'one';
+// (() => {
+//     console.log(alo);
+//     // var alo = 'two'
+// })();
+
+// function one() {
+//     var alo = 'alo'
+//     two()
+// }
+// function two() {
+//     console.log(alo);
+// }
+
+// one()
+// var i = 10;
+// for (var i = 0; i < 4; i++) {}
+// console.log(i);
+
+
+// let a = 7
+var b = 8
+let c = 7
+
+{
+    let c = 8
+}
+// const one = 'one'
+//     console.log(one);
+//     console.log(this.one);
+// function alo() {
+// }
+// alo()
